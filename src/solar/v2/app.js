4a0c531caf424f6108e715cb388eb2df13f7cc47
nykolyn <nykolynandrii@gmail.com>
2022-04-10 17:43:11 +0300

    add colors, rotation animation

--- src/solar/v2/app.js
+++ src/solar/v2/app.js
@@ -1,35 +1,45 @@
-const COORD_WIDTH = 1000;
-const COORD_HEIGHT = 1000;
-
-const SVG = document.querySelector("svg.solar-system");
-const SOLAR_SYSTEM_GROUP = SVG.querySelector(".solar-system__group");
+const COORDS = {
+  WIDTH: 1000,
+  HEIGHT: 1000,
+};
 
 const COLORS = {
   WHITE: "#fff",
   GREY: "#ccc",
+  SUN: "#ea7b21",
+  MERCURY: "#8e8e8e",
+  VENUS: "#d16730",
 };
 
+const SVG = document.querySelector("svg.solar-system");
+const SOLAR_SYSTEM_GROUP = SVG.querySelector(".solar-system__group");
+
 const config = [
-  { size: 40, distance: 0 },
-  { size: 20, distance: 100 },
-  { size: 10, distance: 200 },
+  { size: 40, distance: 0, speed: 0, color: COLORS.SUN },
+  { size: 20, distance: 80, speed: 5000, color: COLORS.MERCURY },
+  { size: 10, distance: 120, speed: 7000, color: COLORS.VENUS },
 ];
 
-const getPlanet = ({ size, distance }) =>
+const getPlanet = ({ size, distance, speed, color }) =>
   `
-    <circle 
-      cx="${COORD_WIDTH / 2 + distance}" 
-      cy="${COORD_HEIGHT / 2}" 
+    <circle
+      class="planet" 
+      cx="${COORDS.WIDTH / 2 + distance}" 
+      cy="${COORDS.HEIGHT / 2}" 
       r="${size}" 
-      fill="${COLORS.WHITE}"
+      fill="${color}"
+      style="
+        --rotation-angle: ${Math.round(360 * Math.random())}deg;
+        --rotation-speed: ${speed}ms;
+    "
     />
   `;
 
 const getOrbit = (distance) =>
   `
     <circle 
-      cx="${COORD_WIDTH / 2}" 
-      cy="${COORD_HEIGHT / 2}" 
+      cx="${COORDS.WIDTH / 2}" 
+      cy="${COORDS.HEIGHT / 2}" 
       r="${distance}" 
       stroke="${COLORS.GREY}"
       fill="none"
@@ -40,7 +50,7 @@ const draw = (markup) => {
   SOLAR_SYSTEM_GROUP.insertAdjacentHTML("beforeend", markup);
 };
 
-config.forEach(({ size, distance }) => {
+config.forEach(({ size, distance, speed, color }) => {
   draw(getOrbit(distance));
-  draw(getPlanet({ size, distance }));
+  draw(getPlanet({ size, distance, speed, color }));
 });
