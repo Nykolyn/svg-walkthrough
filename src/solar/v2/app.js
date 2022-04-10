7aed5eb39821d595400a7d6935febfb4c08db609
nykolyn <nykolynandrii@gmail.com>
2022-04-10 19:28:47 +0300

    add background star field

--- src/solar/v2/app.js
+++ src/solar/v2/app.js
@@ -32,8 +32,7 @@ const config = [
   { name: "venus", size: 15, distance: 120, speed: 7000, color: COLORS.VENUS },
 ];
 
-const generateRandomInt = (min, max) =>
-  Math.round(Math.random() * (max - min) + min);
+const generateRandomInt = (min, max) => Math.round(Math.random() * (max - min) + min);
 const generateRandomFloat = (min, max) => Math.random() * (max - min) + min;
 
 const getPlanetFilter = ({ color, name, size, distance }) => {
@@ -199,10 +198,46 @@ const getOrbit = (distance) =>
     />
   `;
 
+const getStarField = () => {
+  const id = `star-field-`;
+  return `
+      <defs>
+        <radialGradient id="${id}-gradient-1">
+          <stop offset="0%" stop-color="hsla(${generateRandomFloat(190, 230)}, 100%, 20%, 1)" />
+          <stop offset="100%" stop-color="hsla(${generateRandomFloat(290, 360)}, 100%, 20%, 1)" />
+        </radialGradient>
+      </defs>
+      <rect x="0%" y="0%" width="150%" height="200%" fill="url(#${id}-gradient-1)"/>
+      <defs>
+        <radialGradient id="${id}-gradient-2">
+          <stop offset="0%" stop-color="hsla(${generateRandomFloat(190, 230)}, 100%, 20%, 1)" />
+          <stop offset="100%" stop-color="hsla(${generateRandomFloat(290, 360)}, 100%, 20%, 1)" />
+        </radialGradient>
+      </defs>
+      <rect x="100%" y="150%" width="150%" height="200%" fill="url(#${id}-gradient-2)" opacity="0.75"/>
+  
+      <filter id="${id}-bg">
+        <feTurbulence type="fractalNoise" baseFrequency="0.01" seed="${generateRandomFloat(0, 100)}"/> 
+          <feGaussianBlur stdDeviation="10"/>       
+      </filter>
+      <rect width="100%" height="100%" filter="url(#${id}-bg)" opacity="0.3"/>
+  
+      <filter id="${id}-stars">
+        <feTurbulence baseFrequency="0.2" seed="${generateRandomFloat(0, 100)}"/>
+        <feColorMatrix values="0 0 0 9 -4
+                              0 0 0 9 -4
+                              0 0 0 9 -4
+                              0 0 0 0 0.5"/>
+      </filter>
+      <rect width="100%" height="100%" filter="url(#${id}-stars)"/>
+    `;
+};
+
 const draw = (markup) => {
   SOLAR_SYSTEM_GROUP.insertAdjacentHTML("beforeend", markup);
 };
 
+// draw(getStarField());
 draw(getSun({ size: 40 }));
 config.forEach(({ size, distance, speed, color, name }) => {
   draw(getOrbit(distance));
